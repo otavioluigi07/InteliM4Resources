d35dac1170baf0fdcfd76a12381903debc422aa9
otavioluigi07 <Luigi.Macedo@sou.inteli.edu.br>
2023-10-19 11:33:12 -0300

    exercicio1_LuigiOtavio

---
+++ SEMANA_01/01_CODIGOS/sketch_oct16a/sketch_oct16a.ino
@@ -0,0 +1,44 @@
+void setup() {
+
+  pinMode(26, OUTPUT);
+  pinMode(33, OUTPUT);
+  pinMode(14, OUTPUT);
+
+
+}
+
+void loop() {
+
+  digitalWrite(26, HIGH);
+  delay(150);
+  digitalWrite(26, LOW);
+  delay(150);
+
+  digitalWrite(33, HIGH);
+  delay(150);
+  digitalWrite(33, LOW);
+  delay(150);
+
+  digitalWrite(14, HIGH);
+  delay(150);
+  digitalWrite(14, LOW);
+  delay(150);
+
+
+
+  digitalWrite(14, HIGH);
+  delay(150);
+  digitalWrite(14, LOW);
+  delay(150);
+
+  digitalWrite(33, HIGH);
+  delay(150);
+  digitalWrite(33, LOW);
+  delay(150);
+
+  digitalWrite(26, HIGH);
+  delay(150);
+  digitalWrite(26, LOW);
+  delay(150);
+
+}
